04a8209be3a5d7170d93b19729be820475ab3f4d
Ramil <ram4ik@hotmail.com>
2022-02-23 20:43:24 +0200

    Initial Commit

---
+++ ObservableStructsInSwiftUI/ObservableStructsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableStructsInSwiftUIApp.swift
+//  ObservableStructsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ObservableStructsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
